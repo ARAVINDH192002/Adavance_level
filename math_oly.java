c17e62413f0d50b97ffb6d24bf9c2e525adcc690
ARAVINDH.K <19ee004@kpriet.ac.in>
2021-12-16 14:49:42 +0530

    Update math_oly.java

--- math_oly.java
+++ math_oly.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class TT {
+    static long summation(long n) {
+        return (n * (n + 1) / 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        int m = sc.nextInt();
+        sc.close();
+        while (m != 0) {
+            n = summation(n);
+            --m;
+        }
+        System.out.println(n);
+    }
+}
