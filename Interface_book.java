7c1ca3201c2ef33c4c3b0b7188732719610ef60b
ARAVINDH.K <19ee004@kpriet.ac.in>
2021-12-29 15:59:28 +0530

    Update Interface_book.java

--- Interface_book.java
+++ Interface_book.java
@@ -4,32 +4,32 @@ import java.util.Scanner;
     }
 
     class MyBook implements Book {
-    String title;
-    String author;
-    int price;
+        String title;
+        String author;
+        int price;
 
     MyBook(String title, String author, int price) {
-    this.title = title;
-    this.author = author;
-    this.price = price;
+        this.title = title;
+        this.author = author;
+        this.price = price;
     }
 
     @Override
     public void display() {
-    System.out.println("Title: " + title);
-    System.out.println("Author: " + author);
-    System.out.println("Price: " + price);
-    }
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+        }
     }
 
     class Main {
-    public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    String title = sc.nextLine();
-    String author = sc.nextLine();
-    int price = sc.nextInt();
-    MyBook x = new MyBook(title, author, price);
-    x.display();
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String title = sc.nextLine();
+        String author = sc.nextLine();
+        int price = sc.nextInt();
+        MyBook x = new MyBook(title, author, price);
+        x.display();
     }
     }
 
